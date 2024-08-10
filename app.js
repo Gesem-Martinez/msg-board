8c368acf3de88caeec2a6fb426c59ba8df7e6686
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-08-09 22:58:57 -0600

    Setup port for adapatable

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const indexRouter = require('./routes/indexRouter');
 const newRouter = require('./routes/newRouter');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
